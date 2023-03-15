e07b800cbac24040700053d011a8caa13e188946
David Ospina <J_do_o13@hotmail.com>
2023-03-15 12:23:59 -0500

    Model, Route, Controller for Work
    se añade todo el sistema de vista controlador para las asignaciones

---
+++ Back/Models/Works.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const WorkSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        id_employee: {
+            type: String,
+            required: true,
+            trim: true,
+        }, 
+        id_phone: {
+            type: String,
+            required: true,
+            trim: true,
+        }, 
+        date: {
+            type: Date,
+            default: Date.now,
+            required: true,
+            trim: true,
+        }, 
+        Observation: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+    },
+);
+
+export const Work = model('work', WorkSchema);
---
+++ Back/Routes/WorkRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { body } from "express-validator";
+import { AddWork, FindWork, FindAllWork } from "../Controllers/WorkController.js";
+
+const router = express.Router();
+
+router.post('/add', AddWork);
+router.get('/FindWork', FindWork);
+router.get('/FindAllWork', FindAllWork);
+
+export default router;
